ae8c5f7b94b10d74b09060f993e4170fe3ed3f25
Dana Wortman <dwortman@uccs.edu>
2015-09-23 16:13:58 -0600

    Initial commit of Unity project.

---
+++ RollABall_Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{	
+		public float speed;
+		public Text text;
+		private int count;
+	
+		void Start ()
+		{
+				count = 0;
+				text.text = "Count: 0";
+		}
+	
+		void FixedUpdate ()
+		{
+				float horizontal = Input.GetAxis ("Horizontal");
+				float vertical = Input.GetAxis ("Vertical");
+		
+				Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+		
+				rigidbody.AddForce (movement * speed * Time.deltaTime);
+		}
+	
+		void OnTriggerEnter (Collider other)
+		{
+				//Destroy (other.gameObject);
+		
+				if (other.gameObject.tag == "PickUp") {
+						other.gameObject.SetActive (false);
+						++count;
+						text.text = "Count: " + count.ToString ();
+				}
+		}
+}
